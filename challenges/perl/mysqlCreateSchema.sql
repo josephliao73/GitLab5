f6fcfd4563b79abf13aec6477b0bb4410c43ce36
Joseph Liao <josephliao73@gmail.com>
2025-06-06 18:17:15 +0000

    perl to python challenge

--- challenges/perl/mysqlCreateSchema.sql
+++ challenges/perl/mysqlCreateSchema.sql
@@ -1,9 +1,11 @@
+
+
 CREATE TABLE `nasdaq_prices` (
-`ticker` varchar(5),
-`date` int(8),
-`open` decimal(7,2),
-`high` decimal(7,2),
-`low` decimal(7,2),
-`close` decimal(7,2),
-`vol` int(8)
+ `ticker` varchar(4),
+ `date` int(8),
+ `open` decimal(4,2),
+ `high` decimal(4,2),
+ `low` decimal(3,1),
+ `close` decimal(4,2),
+ `vol` int(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
